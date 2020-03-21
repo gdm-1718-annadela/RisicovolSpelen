84dd8cd391506e98038e2052e695a7a1ca45ba37
gdm-1718-annadela <annadela@student.arteveldehs.be>
2020-03-21 14:40:49 +0100

    update icon

--- web/wp/wp-content/themes/risicovolSpelen/footer.php
+++ web/wp/wp-content/themes/risicovolSpelen/footer.php
@@ -2,7 +2,7 @@
 <div class="bg-logo"></div>
 <div id="nav" class="nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></div>
 <div class="second-footer">
-<div id="nav" class="nav-footer red-title second-nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'login-menu' ) ); ?></div>
+<div id="nav" class="nav-footer second-nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'login-menu' ) ); ?></div>
 <div id="nav" class="nav-footer second-nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'second-menu' ) ); ?></div>
 <div id="nav" class="nav-footer second-nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'tool-menu' ) ); ?></div>
 <div id="nav" class="nav-footer second-nav-footer"><?php wp_nav_menu( array( 'theme_location' => 'vorming-menu' ) ); ?></div>
