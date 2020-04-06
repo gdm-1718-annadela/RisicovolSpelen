7813ffdfb01f722a42c6915409235c657073458e
gdm-1718-annadela <annadela@student.arteveldehs.be>
2020-04-06 10:54:56 +0200

    delete

--- web/wp/wp-content/themes/risicovolSpelen/sidebar.php
+++
@@ -1,4 +0,0 @@
-<a href="https://www.facebook.com/groups/risicovolspelen/"><div class="sidebar">
-    <i class="fab fa-facebook"></i>
-<div class="rotate"><p>©RePLAY risicovol spelen Arteveldehogeschool</p></div>
-</div></a>
